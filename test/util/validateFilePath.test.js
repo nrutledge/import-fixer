308105ab053d16964a4313ca091dbbe896cbd95a
benjmhart <benjmhart@gmail.com>
2019-05-24 22:27:17 -0400

    cleanup

--- test/util/validateFilePath.test.js
+++ test/util/validateFilePath.test.js
@@ -11,7 +11,6 @@ describe('validateFilePath', () => {
   it('invalid file path', done => {
     // expect(true);
     const fut1 = validateFilePath('./bs/bs.js');
-    console.log('test running');
     fut1.fork(
       s => {
         expect(s).toEqual(Se.Left('Invalid file path'));
@@ -26,22 +25,15 @@ describe('validateFilePath', () => {
   it('valid file path', done => {
     const path = './test/samples/simple.js';
     const fut2 = validateFilePath(path);
-    console.log('test running');
     fut2.fork(
       s => {
-        console.log('success callback');
         expect(s).toEqual(Se.Right(path));
         done();
       },
       e => {
-        console.log('error callback', e);
         expect(false).toEqual(true);
         done();
       }
     );
   });
-  // it('valid file path', async () => {
-  //   const path = './test/samples/simple.js';
-  //   expect(await validateFilePath(path)).toEqual(Se.Right(path));
-  // });
 });
