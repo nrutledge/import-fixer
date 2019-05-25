c99fe3b0c6934cc10fba73c08a3f6c24306ba76e
benjmhart <benjmhart@gmail.com>
2019-05-24 21:50:02 -0400

    TEST: refactored filePath validator to use Flutures

--- test/util/validateFilePath.test.js
+++ test/util/validateFilePath.test.js
@@ -1,14 +1,47 @@
 const Se = require('sanctuary-either');
+const { env: flutureEnv } = require('fluture-sanctuary-types');
+const { create, env } = require('sanctuary');
+
+const Fluture = require('fluture');
+const S = create({ checkTypes: true, env: env.concat(flutureEnv) });
 
 const { validateFilePath } = require('../../util/validateFilePath');
 
 describe('validateFilePath', () => {
-  it('invalid file path', async () => {
-    expect(await validateFilePath('./bs/bs.js')).toEqual(Se.Left('Invalid file path'));
+  it('invalid file path', done => {
+    // expect(true);
+    const fut1 = validateFilePath('./bs/bs.js');
+    console.log('test running');
+    fut1.fork(
+      s => {
+        expect(s).toEqual(Se.Left('Invalid file path'));
+        done();
+      },
+      e => {
+        throw new Error();
+        done();
+      }
+    );
   });
-
-  it('valid file path', async () => {
+  it('valid file path', done => {
     const path = './test/samples/simple.js';
-    expect(await validateFilePath(path)).toEqual(Se.Right(path));
+    const fut2 = validateFilePath(path);
+    console.log('test running');
+    fut2.fork(
+      s => {
+        console.log('success callback');
+        expect(s).toEqual(Se.Right(path));
+        done();
+      },
+      e => {
+        console.log('error callback', e);
+        expect(false).toEqual(true);
+        done();
+      }
+    );
   });
+  // it('valid file path', async () => {
+  //   const path = './test/samples/simple.js';
+  //   expect(await validateFilePath(path)).toEqual(Se.Right(path));
+  // });
 });
--- util/validateFilePath.js
+++ util/validateFilePath.js
@@ -1,12 +1,17 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 const Se = require('sanctuary-either');
+const { Future } = require('fluture');
 
 //validateFilePath :: String -> Either String String
-module.exports.validateFilePath = async filePath => {
-  try {
-    const result = await fs.exists(filePath);
-    return result ? Se.Right(filePath) : Se.Left('Invalid file path');
-  } catch (err) {
-    return Se.Left(err.toString());
-  }
+module.exports.validateFilePath = filePath => {
+  return Future((resolve, reject) => {
+    const result = fs.exists(filePath, (result, err) => {
+      if (err) {
+        console.log('error returned', err);
+        resolve(Se.Left(err.toString()));
+      }
+
+      resolve(result ? Se.Right(filePath) : Se.Left('Invalid file path'));
+    });
+  });
 };
